0bf6659e4e27119848b720785e4f532342afd398
Cooper Goodman <c-goodman@tamu.edu>
2024-09-24 19:42:16 -0400

    todo refactor main form

--- mk_form/forms.py
+++ mk_form/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from mk_form.models import Data
 
-
+# TODO: Use forms.Form instead, no 'fields = "__all__"'
 class DataModelForm(forms.ModelForm):
     class Meta:
         model = Data
