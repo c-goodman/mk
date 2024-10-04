dc679f2b34876b19bccdf41365530dc8fe26115f
Cooper Goodman <c-goodman@tamu.edu>
2024-10-04 19:27:03 -0400

    Forms input for character fields, helper function for multi-field validation

--- mk_form/forms.py
+++ mk_form/forms.py
@@ -1,10 +1,13 @@
+from typing import Literal, Any
+
 from django import forms
 from django.core.exceptions import ValidationError
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Row, Column, HTML, Submit
+
 # from crispy_bootstrap5.bootstrap5 import BS5Accordion
 
-from mk_form.models import Data, Player, GameTypeCategory
+from mk_form.models import Data, Player, GameTypeCategory, CharacterCategory
 
 
 # TODO: Use forms.Form instead, no 'fields = "__all__"'
@@ -22,41 +25,108 @@ class DataModelForm(forms.ModelForm):
         player_third = cleaned_data.get("player_third")
         player_fourth = cleaned_data.get("player_fourth")
 
+        character_first = cleaned_data.get("character_first")
+        character_second = cleaned_data.get("character_second")
+        character_third = cleaned_data.get("character_third")
+        character_fourth = cleaned_data.get("character_fourth")
+
         game_type = cleaned_data.get("game_type")
+
+        def validate_game_fields(
+            fields: list[str],
+            condition: Any,
+            game_type_variable: Any | None,
+            game_type_value: str,
+            message: str,
+        ) -> None:
+
+            game_condition = (game_type_variable == game_type_value) & condition
+
+            if game_condition:
+                msg = ValidationError(message=f"Game Type: {game_type}, {message}")
+
+                for field in fields:
+                    self.add_error(field, msg)
 
         # Two Player Games
         # 2P: Player First or Second can not be "None"
-        if (game_type == GameTypeCategory.TWO) & (
-            (player_first == "None") | (player_second == "None")
-        ):
-            msg = ValidationError(
-                message=f"Game Type: {GameTypeCategory.TWO}, Player First and/or Second can not be 'None'."
-            )
+        validate_game_fields(
+            fields=["player_first"],
+            condition=(player_first == "None"),
+            game_type_variable=game_type,
+            game_type_value=GameTypeCategory.TWO,
+            message="Player First can not be 'None'.",
+        )
 
-            self.add_error("player_first", msg)
-            self.add_error("player_second", msg)
+        validate_game_fields(
+            fields=["player_second"],
+            condition=(player_second == "None"),
+            game_type_variable=game_type,
+            game_type_value=GameTypeCategory.TWO,
+            message="Player First can not be 'None'.",
+        )
 
         # 2P: Player First can not equal Player Second
-        if (game_type == GameTypeCategory.TWO) & (player_first == player_second):
-            msg = ValidationError(
-                message=f"Game Type: {GameTypeCategory.TWO}, Player First can not be equal to Player Second."
-            )
-
-            self.add_error("game_type", msg)
-            self.add_error("player_first", msg)
-            self.add_error("player_second", msg)
+        validate_game_fields(
+            fields=["player_first", "player_second"],
+            condition=(player_first == player_second),
+            game_type_variable=game_type,
+            game_type_value=GameTypeCategory.TWO,
+            message="Player First can not be equal to Player Second.",
+        )
 
         # 2P: Player Third and Fourth must be 'None'
+        validate_game_fields(
+            fields=["player_third"],
+            condition=(player_third != "None"),
+            game_type_variable=game_type,
+            game_type_value=GameTypeCategory.TWO,
+            message="Player Third must be 'None'.",
+        )
+
+        validate_game_fields(
+            fields=["player_fourth"],
+            condition=(player_fourth != "None"),
+            game_type_variable=game_type,
+            game_type_value=GameTypeCategory.TWO,
+            message="Player Fourth must be 'None'.",
+        )
+
+        # 2P: Character First can not equal Character Second
         if (game_type == GameTypeCategory.TWO) & (
-            (player_third != "None") | (player_fourth != "None")
+            (character_first == character_second)
         ):
             msg = ValidationError(
-                message=f"Game Type: {GameTypeCategory.TWO}, Player Third and Fourth must be 'None'."
+                message=f"Game Type: {GameTypeCategory.TWO}, Character First can not be equal to Character Second."
             )
 
             self.add_error("game_type", msg)
-            self.add_error("player_third", msg)
-            self.add_error("player_fourth", msg)
+            self.add_error("character_first", msg)
+            self.add_error("character_second", msg)
+
+        # 2P: Character Third and Fourth can not be 'None'
+        if (game_type == GameTypeCategory.TWO) & (
+            (character_first is None) | (character_second is None)
+        ):
+            msg = ValidationError(
+                message=f"Game Type: {GameTypeCategory.TWO}, Character First and/or Second can not be 'None'."
+            )
+
+            self.add_error("game_type", msg)
+            self.add_error("character_first", msg)
+            self.add_error("character_second", msg)
+
+        # 2P: Character Third and Fourth must be 'None'
+        if (game_type == GameTypeCategory.TWO) & (
+            (character_third is not None) | (character_fourth is not None)
+        ):
+            msg = ValidationError(
+                message=f"Game Type: {GameTypeCategory.TWO}, Character Third and Fourth must be 'None'."
+            )
+
+            self.add_error("game_type", msg)
+            self.add_error("character_third", msg)
+            self.add_error("character_fourth", msg)
 
         # 3 Player Games
         # 3P: Player First, Second or Third can not be "None"
@@ -177,6 +247,18 @@ class DataModelForm(forms.ModelForm):
                     ),
                     css_class="container",
                 ),
+                HTML("<br>"),
+                Div(
+                    Row(HTML("<h4>Players</h4>")),
+                    Row(
+                        Column("character_first", css_class="col me-auto"),
+                        Column("character_second", css_class="col me-auto"),
+                        Column("character_third", css_class="col me-auto"),
+                        Column("character_fourth", css_class="col me-auto"),
+                        css_class="align-items-end justify-content-center",
+                    ),
+                    css_class="container",
+                ),
                 css_class="form-group",
             ),
             HTML("<br>"),
@@ -198,4 +280,8 @@ class DataModelForm(forms.ModelForm):
             "player_second",
             "player_third",
             "player_fourth",
+            "character_first",
+            "character_second",
+            "character_third",
+            "character_fourth",
         ]
