4b614957dc258d8c15e5e05c8cb999f0542f7158
Cooper Goodman <c-goodman@tamu.edu>
2024-10-04 17:41:32 -0400

    Test response for form and list views

--- mk_form/tests/test_mk_form_views.py
+++ mk_form/tests/test_mk_form_views.py
@@ -6,3 +6,19 @@ def test_home_endpoint_response(client):
     response = client.get(path="/")
     assert 200 == response.status_code
     assert ":D" in str(response.content)
+
+
+@pytest.mark.functional
+@pytest.mark.django_db
+def test_form_endpoint_response(client):
+    response = client.get(path="/data_form")
+    assert 200 == response.status_code
+    assert "Enter Data" in str(response.content)
+
+
+@pytest.mark.functional
+@pytest.mark.django_db
+def test_data_list_endpoint_response(client):
+    response = client.get(path="/data_list")
+    assert 200 == response.status_code
+    assert "Recorded Data" in str(response.content)
